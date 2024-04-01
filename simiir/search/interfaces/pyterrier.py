e3c0c0773a044f4bc16e0da99ee2f932f1fabe2e
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-04-01 13:05:12 +0100

    documentation and class methods

--- simiir/search/interfaces/pyterrier.py
+++ simiir/search/interfaces/pyterrier.py
@@ -1,24 +1,40 @@
-from simiir.search_interfaces.base_interface import BaseSearchInterface
+from simiir.search.interfaces.base import BaseSearchInterface
 import logging
 
-log = logging.getLogger('simuser.search_interfaces.pyterrier_interface')
+log = logging.getLogger('simuser.search.interfaces.pyterrier')
 
 class PyTerrierSearchInterface(BaseSearchInterface):
+    '''
+    Interface for using PyTerrier for BatchRetrieval or Meta Index access
+
+    Parameters:
+    index_or_dir : str
+        Reference to PyTerrier Index
+    wmodel : str
+        Reference to PyTerrier weighting model
+    controls : dict
+        Controls for the weighting model
+    properties : dict
+        Properties for the weighting model
+    text_field : str
+        Field in the index to use as the text field
+    memory : bool
+        Whether to load the index into memory
+    '''
     def __init__(self, 
-                 dataset : str, 
+                 index_or_dir : str, 
                  wmodel : str = None, 
                  controls : dict = None, 
                  properties : dict = None, 
-                 variant : str = 'terrier_stemmed_text', 
                  text_field : str = 'body', 
-                 memory=False
+                 memory : bool = False,
                  ):
         super().__init__()
         import pyterrier as pt
         if not pt.started():
             pt.init()
 
-        self.__index = pt.IndexFactory.of(pt.get_dataset(dataset).get_index(variant=variant), memory=memory)
+        self.__index = pt.IndexFactory.of(index_or_dir, memory=memory)
         self.__reader = self.__index.getMetaIndex()
 
         if self.__reader is None: log.warning("No reader defined, cannot fetch document text, doing so will result in an error")
@@ -29,6 +45,21 @@ class PyTerrierSearchInterface(BaseSearchInterface):
         
         if wmodel is not None: self.__engine = pt.BatchRetrieve(self.__index, wmodel, controls=controls, properties=properties)
         else: self.__engine = None
+
+    @classmethod
+    def from_dataset(cls, 
+                     dataset : str, 
+                     variant : str = 'terrier_stemmed_text', 
+                     wmodel : str = None, 
+                     controls : dict = None, 
+                     properties : dict = None, 
+                     text_field : str = 'body', 
+                     memory : bool = False):
+        import pyterrier as pt
+        if not pt.started():
+            pt.init()
+        index_ref = pt.get_dataset(dataset).get_index(variant=variant)
+        return cls(index_ref, wmodel=wmodel, controls=controls, properties=properties, text_field=text_field, memory=memory)
     
     def issue_query(self, query, top=100):
         assert self.__engine is not None, "No engine defined"
