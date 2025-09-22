a2ed6a59231cffb0d7a6bcd74827efb49a0dd033
Saber Zerhoudi <saber.zerhoudi@uni-passau.de>
2025-09-22 09:28:37 +0200

    feat: add new component 'test'

---
+++ simiir/search/interfaces/test.py
@@ -0,0 +1,12 @@
+class Test(object):
+    """
+    test component.
+    """
+    def __init__(self, configuration):
+        pass
+
+    def execute(self):
+        """
+        Main execution method.
+        """
+        pass
